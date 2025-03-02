f880593fe964fd63f6eaeda07d5a24d6486630f0
Saurav Khanal <sauravkhanal44@gmail.com>
2025-03-02 10:18:54 +0545

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-yc4l%mv7+sx3h-_a503o8ety)b*uhjy!6s)ytpk0024xejvra7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['unsocial-media.onrender.com', '13.228.225.19', 'localhost']
+ALLOWED_HOSTS = ['unsocial-media.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
